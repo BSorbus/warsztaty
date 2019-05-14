5bed8f54c442ae56742e4fca70d3b84616c8c13d
BSorbus <BSorbus@gmail.com>
2019-05-14 23:59:56 +0200

    Better Views

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,30 +1,11 @@
 class ChartsController < ApplicationController
-
-  PROVINCES = [['02', 'dolnośląskie'],
-               ['04', 'kujawsko-pomorskie'],
-               ['06', 'lubelskie'],
-               ['08', 'lubuskie'],
-               ['10', 'łódzkie'],
-               ['12', 'małopolskie'],
-               ['14', 'mazowieckie'],
-               ['16', 'opolskie'],
-               ['18', 'podkarpackie'],
-               ['20', 'podlaskie'],
-               ['22', 'pomorskie'],
-               ['24', 'śląskie'],
-               ['26', 'świętokrzyskie'],
-               ['28', 'warmińsko-mazurskie'],
-               ['30', 'wielkopolskie'],
-               ['32', 'zachodniopomorskie']]
-
-
   before_action :authenticate_user!
 
   helper :all
 
   def departments_by_question_pie
     data_array = []
-    Department.where.not(id: [1,2]).order(:short).each do |dep|
+    Department.where.not(id: [1,2,3]).order(:short).each do |dep|
       data_array <<  ["#{dep.short}", 
                       sum_answer(dep, "#{params[:question]}") ]
     end
@@ -33,7 +14,7 @@ class ChartsController < ApplicationController
 
   def departments_sum_pie
     data_array = []
-    Department.where.not(id: [1,2]).order(:short).each do |dep|
+    Department.where.not(id: [1,2,3]).order(:short).each do |dep|
       data_array << ["#{dep.short}", 
                       sum_answer(dep, "1") + 
                       sum_answer(dep, "2") + 
@@ -45,7 +26,7 @@ class ChartsController < ApplicationController
 
   def departments_by_question_column
     data_array = [] 
-    Department.where.not(id: [1,2]).order(:short).each do |department|  
+    Department.where.not(id: [1,2,3]).order(:short).each do |department|  
       for answer in "#{params[:question]}".."#{params[:question]}" do
         data_array << { name: "#{department.short}", 
                         data: {"Pytanie #{answer}": sum_answer(department.id, answer) }
@@ -58,7 +39,7 @@ class ChartsController < ApplicationController
 
   def departments_sum_column
     data_array = [] 
-    Department.where.not(id: [1,2]).order(:short).each do |department|  
+    Department.where.not(id: [1,2,3]).order(:short).each do |department|  
       data_array << { name: "#{department.short}", 
                       data: {"Wszystkie pytania": sum_answer(department.id, "1") + 
                                                   sum_answer(department.id, "2") +
@@ -89,26 +70,6 @@ class ChartsController < ApplicationController
       (Survey.where(answer_4_place_3: dep).count * 1) 
     end
 
-  end
-
-  def point_files
-    data_array = []
-    PROVINCES.each do |province|
-      data_array << [ province[0], PointFile.where(status: [:active], oi_4: province[1].mb_chars.upcase).count ]
-    end
-    render json: data_array.to_json
-  end
-
-  def xml_miejsce_realizacji_tables
-    data_array = []
-    PROVINCES.each do |province|
-      data_array << [ province[0], XmlMiejsceRealizacjiTable.joins(xml_partner_table: [:proposal_file]).where(wojewodztwo_opis: province[1].mb_chars.upcase, xml_partner_tables: {proposal_files: {status: [:active]}}).count ]
-    end
-    render json: data_array.to_json
-  end
-
-
-  private
-    
+  end    
 
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,5 @@
 class StaticPagesController < ApplicationController
+  before_action :authenticate_user! , except: [:home]
 
   caches_page :home, :introduction, :job_1, :job_2, :job_3, :job_4, :job_5, :gzip => :best_speed
 
